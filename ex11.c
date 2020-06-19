b710b7f5856847ede7f5bbc0051a636a27a267e8
Austin Reed <ausmreed@gmail.com>
2020-06-19 15:49:23 -0400

    Added some extra commentary on ex11 strings.

--- ex11.c
+++ ex11.c
@@ -2,44 +2,45 @@
 
 int main(int argc, char *argv[])
 {
-    int numbers[4] = { 0 }; 
+    int numbers[4] = { 0 };
     char name[4] = { 'a' };
 
     // first, print them out raw
-    printf("numbers: %d %d %d %d \n", numbers[0], numbers[1], numbers[2], numbers[3]); 
+    printf("numbers: %d %d %d %d \n", numbers[0], numbers[1], numbers[2], numbers[3]);
 
-    printf("name each: %c %c %c %c \n", name[0], name[1], name[2], name[3]); 
+    printf("name each: %c %c %c %c \n", name[0], name[1], name[2], name[3]);
 
-    printf("name: %s \n", name); 
+    printf("name: %s \n", name);
 
-    // setup the numbers 
+    // setup the numbers
 
-    numbers[0] = 1; 
-    numbers[1] = 2; 
-    numbers[2] = 3; 
-    numbers[3] = 4; 
-     
-     // setup the name 
-     name[0] = 'A'; 
-     name[1] = 'u'; 
+    numbers[0] = 1;
+    numbers[1] = 2;
+    numbers[2] = 3;
+    numbers[3] = 4;
+
+     // setup the name
+     name[0] = 'A';
+     name[1] = 'u';
      name[2] = 's';
-     name[3] =  '\0'; 
+     name[3] =  '\0';
+     printf("the last character is %c \n", name[3]);
+     printf("Notice, there's no character there! \n");
+     // then print them out initialized.
+     printf("Numbers: %d %d %d %d \n", numbers[0], numbers[1], numbers[2], numbers[3]);
 
-     // then print them out initialized. 
-     printf("Numbers: %d %d %d %d \n", numbers[0], numbers[1], numbers[2], numbers[3]); 
+     printf("Name each: %c %c %c %c \n", name[0], name[1], name[2], name[3]);
 
-     printf("Name each: %c %c %c %c \n", name[0], name[1], name[2], name[3]); 
+     // print the name like a string.
+     printf("name: %s \n", name);
 
-     // print the name like a string. 
-     printf("name: %s \n", name); 
-
-    //another way to use name. 
-    char *another = "Aus"; 
+    //another way to use name.
+    char *another = "Aus";
 
     printf("another: %s \n", another);
 
-    printf("another each: %c %c %c %c \n", another[0], another[1], another[2], another[3]); 
+    printf("another each: %c %c %c %c \n", another[0], another[1], another[2], another[3]);
 
-    return 0; 
-    
+    return 0;
+
 }
