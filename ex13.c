0e2e927be0a5597bd0a952101d8438a2da367cab
Austin Reed <ausmreed@gmail.com>
2020-06-23 14:03:16 -0400

    Ex13 Added

---
+++ ex13.c
@@ -0,0 +1,23 @@
+// Example 13, iterating through strings. Similar to some experimentation
+// I was doing in Ex 12.
+
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+  int i = 0;
+  // go through each string in argv
+  // Why am I skipping argv[0]?
+  for (i = 1; i < argc; i++) {
+    printf("arg %d: %s \n", i, argv[i]);
+  }
+
+  // Let's make our own array of strings
+  char *states[] = {
+    "California", "Oregon", "Washington", "Texas"
+  };
+  int num_states = 4;
+  for (i = 0; i < num_states; i++) {
+    printf("State %d: %s \n", i, states[i]);
+  }
+  return 0;
+}
